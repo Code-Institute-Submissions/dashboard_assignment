138f40c31544ec26891f9bdeb569738ebeecf4bd
Martin Curran <martincurran28@hotmail.com>
2019-04-07 17:27:26 +0000

    Created the first graph (pie chart) using a simple gender-balance function to start with.

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -4,4 +4,22 @@ queue()
     
 function makeGraphs(error, resultsData){
     var ndx = crossfilter(resultsData);
+    
+    show_gender_balance(ndx);
+    
+    dc.renderAll();
+    
 }    
+
+function show_gender_balance(ndx){
+    var dim = ndx.dimension(dc.pluck('sex'));
+    var group = dim.group();
+    
+    dc.pieChart("#gender-balance")
+        .height(300)
+        .radius(120)
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(1500);
+    
+}
