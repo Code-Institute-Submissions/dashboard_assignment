a0263bdbedb1b15005f5e5a94b0521933a1ef4f5
Martin Curran <martincurran28@hotmail.com>
2019-04-10 13:48:56 +0000

    Created first test with Jasmine for maths related to the Gender Balance pie Chart.

---
+++ scripts/calc.js
@@ -0,0 +1,9 @@
+//Gender Balance Pie Chart Numbers//
+
+describe ("Calculator", function(){
+    describe("Addition test", function(){
+        it ("should return 59", function(){
+            expect(addition(20, 39)).toBe(59);
+        });
+    })
+})
